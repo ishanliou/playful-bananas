6c49a19640140d7f3261b7908b1d4db43eed22ee
I Shan Liou <ishanliou@gmail.com>
2023-08-08 23:57:55 -0700

    bananas

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,47 @@ import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { DepthOfField, EffectComposer } from "@react-three/postprocessing";
 import { Suspense, useRef, useState } from "react";
 import * as THREE from "three";
-import Banana from "./Banana";
+// import Banana from "./Banana";
 
-// function Banana({ z }) {
-//   const ref = useRef();
-//   const { nodes, materials } = useGLTF("/banana-transformed.glb");
+function Banana({ z }) {
+  const ref = useRef();
+  const { nodes, materials } = useGLTF("/banana-transformed.glb");
 
-//   const { viewport, camera } = useThree();
-//   const { width, height } = viewport.getCurrentViewport(camera, [0, 0, -z]);
+  const { viewport, camera } = useThree();
+  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, -z]);
 
-//   const [data] = useState({
-//     x: THREE.MathUtils.randFloatSpread(2),
-//     y: THREE.MathUtils.randFloatSpread(height * 2),
-//     rX: Math.random() * Math.PI,
-//     rY: Math.random() * Math.PI,
-//     rZ: Math.random() * Math.PI,
-//   });
+  const [data] = useState({
+    x: THREE.MathUtils.randFloatSpread(2),
+    y: THREE.MathUtils.randFloatSpread(height * 2),
+    rX: Math.random() * Math.PI,
+    rY: Math.random() * Math.PI,
+    rZ: Math.random() * Math.PI,
+  });
 
-//   useFrame((state) => {
-//     ref.current.rotation.set(
-//       (data.rX += 0.0001),
-//       (data.rY += 0.001),
-//       (data.rZ += 0.0005)
-//     );
-//     ref.current.position.set(data.x * width, (data.y += 0.025), z);
+  useFrame((state) => {
+    ref.current.rotation.set(
+      (data.rX += 0.0001),
+      (data.rY += 0.001),
+      (data.rZ += 0.0005)
+    );
+    ref.current.position.set(data.x * width, (data.y += 0.025), z);
 
-//     if (data.y > height) {
-//       data.y = -height;
-//     }
-//   });
-//   return (
-//     <>
-//       <mesh
-//         ref={ref}
-//         geometry={nodes.banana.geometry}
-//         material={materials.Banana_High}
-//         material-emissive="orange"
-//       />
-//     </>
-//   );
-// }
-// useGLTF.preload("/banana-transformed.glb");
+    if (data.y > height) {
+      data.y = -height;
+    }
+  });
+  return (
+    <>
+      <mesh
+        ref={ref}
+        geometry={nodes.banana.geometry}
+        material={materials.Banana_High}
+        material-emissive="orange"
+      />
+    </>
+  );
+}
+useGLTF.preload("/banana-transformed.glb");
 export default function App({ count = 100, depth = 80 }) {
   return (
     <Canvas
